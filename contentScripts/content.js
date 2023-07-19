aecb33bb107ef34afca0ef45852f2f88d1270394
YeeashaDev <aishaagunbiade05@gmail.com>
2023-07-19 13:17:07 +0100

    text styles

--- contentScripts/content.js
+++ contentScripts/content.js
@@ -11,7 +11,7 @@ function activateTextInspection() {
 
   for (const element of elements) {
     element.addEventListener("mouseover", handleMouseOver);
-    element.addEventListener("mouseout", handleMouseOut);
+    element.addEventListener("mousedown", handleMouseOut);
   }
 }
 
@@ -42,25 +42,30 @@ function displayFontProperties(fontProperties) {
     fontFamilyText += " (Google Font)";
   }
 
-  const lineHeightText =
-    fontProperties.lineHeight === "normal"
-      ? "Line Height<br/> normal"
-      : `Line Height<br/> ${fontProperties.lineHeight}`;
+  // Convert the RGB color to hex format
+  const hexColor = rgbToHex(fontProperties.color);
+  
   div.innerHTML = `
-    <p>Primary Font Family:<br/> ${fontProperties.fontPrimaryFamily}</p>
+    <p>Primary Family<br/> ${fontProperties.fontPrimaryFamily}</p>
+    <hr/>
+    <p>Font Family<br/> ${fontProperties.fontFamily}</p>
     <hr/>
     <div class="size">
       <p>Size<br/> ${fontProperties.fontSize}</p>
       <p>Weight<br/> ${fontProperties.fontWeight}</p>
-      <p>${lineHeightText}</p>
+      <p>Height<br/>${fontProperties.lineHeight}</p>
+      <div>
+      <div class="color-box">
+      <p> Color </p>
+      <div style="display:flex;align-items:center;justify-content:center; flex-direction:row-reverse;">
+      <span class="color-palette" style="background-color: ${fontProperties.color};"></span>
+      <p>${hexColor}</p>
     </div>
-    <hr/>
-    <div>
-      <p class="color-box"> Color:<br/> 
-        <span class="color-palette" style="background-color: ${fontProperties.color};"></span>
-        ${fontProperties.color}
-      </p>
+      </div>
+
+      
     </div>
+
   `;
   document.body.appendChild(div);
 }
@@ -85,124 +90,23 @@ function isGenericFont(fontFamily) {
   return genericFonts.includes(fontFamily.toLowerCase());
 }
 
-// Highlight Text Feature
-let selectedText = null;
-
-document.addEventListener("mouseup", handleMouseUp);
-
-function handleMouseUp() {
-  const text = window.getSelection().toString().trim();
-  if (text !== "") {
-    selectedText = text;
-    showTooltip();
-  } else {
-    hideTooltip();
+// Helper function to convert RGB color to hex format
+function rgbToHex(rgbColor) {
+  const rgbValues = rgbColor.match(/\d+/g);
+  if (rgbValues) {
+    const r = parseInt(rgbValues[0]);
+    const g = parseInt(rgbValues[1]);
+    const b = parseInt(rgbValues[2]);
+    return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
   }
+  return rgbColor;
 }
 
-let tooltip = null;
-
-function showTooltip() {
-  clearFontProperties();
-  if (tooltip) {
-    return; // Return early if the tooltip is already displayed
-  }
-
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-  const rect = range.getBoundingClientRect();
-  const tooltipHeight = rect.height; // Adjust the height of the tooltip as needed
-
-  tooltip = document.createElement("div");
-  tooltip.id = "highlight-tooltip";
-  tooltip.innerHTML = `
-    <button id="highlight-btn">Highlight</button>
-    <button id="copy-btn">Copy</button>
-  `;
-
-  tooltip.style.top = `${tooltipHeight}px`; // Position the tooltip at the bottom of the selected text
-  tooltip.style.left = `${rect.left}px`;
-
-  document.body.appendChild(tooltip);
-
-  const highlightButton = tooltip.querySelector("#highlight-btn");
-  const copyButton = tooltip.querySelector("#copy-btn");
-
-  highlightButton.addEventListener("click", handleHighlightButtonClick);
-  copyButton.addEventListener("click", handleCopyButtonClick);
-
-  const highlightedText = document.createElement("span");
-  range.surroundContents(highlightedText);
-
-  
-  
+// Helper function to convert RGB component to hex
+function componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length === 1 ? "0" + hex : hex;
 }
-
-function handleHighlightButtonClick(event) {
-  event.stopPropagation();
-  highlightSelectedText();
-  clearFontProperties()
-  
-}
-
-function handleCopyButtonClick(event) {
-  event.stopPropagation();
-  copySelectedText();
-  clearFontProperties()
-  
-}
-
-function highlightSelectedText() {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-  const span = document.createElement("span");
-  span.style.backgroundColor = "yellow";
-  range.surroundContents(span);
-}
-
-function copySelectedText() {
-  const selectedText = window.getSelection().toString().trim();
-  if (selectedText !== "") {
-    const dummyInput = document.createElement("input");
-    dummyInput.value = selectedText;
-    document.body.appendChild(dummyInput);
-    dummyInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummyInput);
-  }
-}
-
-function hideTooltip() {
-  if (tooltip) {
-    tooltip.remove();
-    tooltip = null;
-  }
-  selectedText = null;
-}
-
-document.addEventListener("selectionchange", function() {
-  const selection = window.getSelection();
-  if (selection ) {
-    showTooltip();
-  } else {
-    hideTooltip();
-  }
-});
-
-document.addEventListener("mousedown", function(event) {
-  const selection = window.getSelection();
-  if (selection && !selection.isCollapsed && tooltip ) {
-    const tooltipRect = tooltip.getBoundingClientRect();
-    if (
-      event.clientX < tooltipRect.left ||
-      event.clientX > tooltipRect.right ||
-      event.clientY < tooltipRect.top ||
-      event.clientY > tooltipRect.bottom
-    ) {
-      hideTooltip();
-    }
-  }
-});
 
 function clearFontProperties() {
   const div = document.getElementById("text-inspector");
@@ -210,4 +114,3 @@ function clearFontProperties() {
     div.remove();
   }
 }
-
---
+++ contentScripts/highlight/highlight.js
@@ -0,0 +1,118 @@
+// Highlight Text Feature
+let selectedText = null;
+
+document.addEventListener("mouseup", handleMouseUp);
+
+function handleMouseUp() {
+  const text = window.getSelection().toString().trim();
+  if (text !== "") {
+    selectedText = text;
+    showTooltip();
+  } else {
+    hideTooltip();
+  }
+}
+
+let tooltip = null;
+
+function showTooltip() {
+  clearFontProperties();
+  if (tooltip) {
+    return; // Return early if the tooltip is already displayed
+  }
+
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const rect = range.getBoundingClientRect();
+  const tooltipHeight = rect.height; // Adjust the height of the tooltip as needed
+
+  tooltip = document.createElement("div");
+  tooltip.id = "highlight-tooltip";
+  tooltip.innerHTML = `
+    <button id="highlight-btn">Highlight</button>
+    <button id="copy-btn">Copy</button>
+  `;
+
+  tooltip.style.top = `${tooltipHeight}px`; // Position the tooltip at the bottom of the selected text
+  tooltip.style.right = `${rect.right}px`;
+
+  document.body.appendChild(tooltip);
+
+  const highlightButton = tooltip.querySelector("#highlight-btn");
+  const copyButton = tooltip.querySelector("#copy-btn");
+
+  highlightButton.addEventListener("click", handleHighlightButtonClick);
+  copyButton.addEventListener("click", handleCopyButtonClick);
+
+  const highlightedText = document.createElement("span");
+  range.surroundContents(highlightedText);
+}
+
+function handleHighlightButtonClick(event) {
+  event.stopPropagation();
+  highlightSelectedText();
+  clearFontProperties()
+  
+}
+
+function handleCopyButtonClick(event) {
+  event.stopPropagation();
+  copySelectedText();
+  clearFontProperties()
+  
+}
+
+function highlightSelectedText() {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const span = document.createElement("span");
+  span.style.backgroundColor = "yellow";
+  range.surroundContents(span);
+}
+
+function copySelectedText() {
+  const selectedText = window.getSelection().toString().trim();
+  if (selectedText !== "") {
+    const dummyInput = document.createElement("input");
+    dummyInput.value = selectedText;
+    document.body.appendChild(dummyInput);
+    dummyInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummyInput);
+  }
+}
+
+function hideTooltip() {
+  if (tooltip) {
+    tooltip.remove();
+    tooltip = null;
+  }
+  selectedText = null;
+}
+
+document.addEventListener("selectionchange", function() {
+  const selection = window.getSelection();
+  if (selection ) {
+    showTooltip();
+  } else {
+    hideTooltip();
+  }
+});
+
+document.addEventListener("mousedown", function(event) {
+  const selection = window.getSelection();
+  if (selection && !selection.isCollapsed && tooltip ) {
+    const tooltipRect = tooltip.getBoundingClientRect();
+    if (
+      event.clientX < tooltipRect.left ||
+      event.clientX > tooltipRect.right ||
+      event.clientY < tooltipRect.top ||
+      event.clientY > tooltipRect.bottom
+    ) {
+      hideTooltip();
+    }
+  }
+});
+
+
+
